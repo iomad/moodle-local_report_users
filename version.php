7ddf6728da1aa45c95d6dd81c78362378fbd9c23
Derick Turner <derick@e-learndesign.co.uk>
2025-06-16 14:23:43 +0100

    IOMAD 4.5 bring version.php files into line with current standards

--- version.php
+++ version.php
@@ -22,6 +22,9 @@
  */
 
 $plugin->release  = '4.5.5 (Build: 20250609)'; // Human-friendly version name
-$plugin->version  = 2020102700;
-$plugin->requires = 2019052000;
+$plugin->version  = 2024100700;   // The (date) version of this plugin.
+$plugin->requires = 2024100700;   // Requires this Moodle version.
 $plugin->component  = 'local_report_users';
+$plugin->dependencies = ['local_iomad' => 2024090401];
+$plugin->supported = [405, 405];
+$plugin->maturity = MATURITY_STABLE;
